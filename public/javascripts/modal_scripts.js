5e9c727c2bc028469a0f072e8044e6d5b2f6c6f3
yvette <ybm2@rice.edu>
2020-07-06 13:59:08 -0500

    updated new/update user modal

--- public/javascripts/modal_scripts.js
+++ public/javascripts/modal_scripts.js
@@ -2,6 +2,7 @@ function UpdateUserModal() {
    $('#updateUserModal').on('show.bs.modal', function (event) {
     var button = $(event.relatedTarget) // Button that triggered the modal
     var user_info = button.data('id') // Extract info from data-* attributes
+    console.log(user_info)
     
     // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
     var modal = $(this)
@@ -9,10 +10,28 @@ function UpdateUserModal() {
     modal.find('.modal-body input[name="FirstName"]').val(user_info.first_name)
     modal.find('.modal-body input[name="LastName"]').val(user_info.last_name)
     modal.find('.modal-body input[name="Email"]').val(user_info.email)
-    modal.find('.modal-body input[name="Position"]').val(user_info.position)
     modal.find('.modal-body input[name="Institute"]').val(user_info.institute)
     modal.find('.modal-body input[name="Time"]').val(user_info.percent_xenon)
     modal.find('.modal-body input[name="Tasks"]').val(user_info.tasks)
     modal.find('.modal-body input[name="StartDate"]').val(new Date(user_info.start_date).toISOString().slice(0,10))
+
+    if (modal.find('.modal-body option[value="'+ user_info.position +'"]').length) {
+      modal.find('.modal-body option[id="selected"]').attr('selected', false)
+      modal.find('.modal-body option[value="'+ user_info.position +'"]').attr('selected', true)
+    }
+    
+    if (user_info.mailing_lists) {
+      var lists = user_info.mailing_lists
+      for (i = 0; i < lists.length; i++) {
+        if (lists[i] != null) {
+          modal.find('.modal-body input[value="'+ lists[i]+ '"]').prop("checked", true)
+        }
+      }
+    }
+  })
+  $('#updateUserModal').on('hidden.bs.modal', function(e) {
+    $("#updateUserModal .modal-body").find('input:checkbox').prop('checked', false);
+    $("#updateUserModal .modal-body").find('option').attr('selected', false);
+    $("#updateUserModal").find('.modal-body option[id="selected"]').attr('selected', true)
   })
 }
--- routes/users.js
+++ routes/users.js
@@ -39,35 +39,37 @@ router.post('/:userid/updateContactInfoAdmin', ensureAuthenticated, (req, res) =
     // Get our form values. These rely on the "name" attributes
     var db = req.test_db
     var user_id = new ObjectId(req.params.userid)
-    var instituteName = req.body.Institute;
-    console.log(req.body.StartDate)
 
-    var enddate = req.body.EndDate
-    if(req.body.EndDate != "") {
-        enddate = new Date(req.body.EndDate)
+    var idoc = {};
+    idoc['first_name'] = req.body.FirstName;
+    idoc['last_name'] = req.body.LastName;
+    idoc['email'] = req.body.Email;
+    idoc['institute'] = req.body.Institute;
+    if(req.body.position != null){
+        idoc['position'] = req.body.position;
     }
-    
+    if(req.body.Time != ""){
+        idoc['percent_xenon'] = req.body.Time;
+    }
+    if(req.body.Tasks != ""){
+        idoc['tasks'] = req.body.Tasks;
+    }
+    idoc['mailing_lists'] = [req.body.mlist1, req.body.mlist2, req.body.mlist3]
+    if(req.body.StartDate != ""){
+        idoc['start_date'] = new Date(`${req.body.StartDate}`);
+    }
+    if(req.body.EndDate != "") {
+        idoc["end_date"] = new Date(req.body.EndDate)
+    }
+
     db.collection('users').findOneAndUpdate(
         {"_id": user_id}, 
-        { 
-            $set: {
-                "first_name": req.body.FirstName,
-                "last_name": req.body.LastName,
-                "email": req.body.Email,
-                "institute": instituteName,
-                "position": req.body.Position,
-                "percent_xenon": req.body.Time,
-                "tasks": req.body.Tasks,
-                "mailing_lists": [req.body.mlist1, req.body.mlist2, req.body.mlist3],
-                "start_date": new Date(`${req.body.StartDate}`),
-                "end_date": enddate,
-            }
-    },
+        {$set: idoc},
     (e, update) => {
         console.log(update)
     })
         console.log(`success. Modified ${req.body.FirstName} ${req.body.LastName}`),
-        res.redirect(`/institutes/${instituteName}`)
+        res.redirect(`/institutes/${req.body.Institute}`)
 })
 
 /* POST req to add a user */
@@ -79,29 +81,24 @@ router.post('/adduser', ensureAuthenticated, (req, res) => {
     var db = req.test_db
 
     // Get our form values. These rely on the "name" attributes
-    var firstName = req.body.FirstName;
-    var lastName = req.body.LastName;
-    var userEmail = req.body.Email;
-    var position = req.body.Position;
-    var instituteName = req.body.Institute;
-    var time = req.body.Time;
-    var tasks = req.body.Tasks;
-    var start = new Date(req.body.StartDate);
+    var idoc = {};
+    idoc['first_name'] = req.body.FirstName;
+    idoc['last_name'] = req.body.LastName;
+    idoc['email'] = req.body.Email;
+    idoc['institute'] = req.body.Institute;
+    idoc['position'] = req.body.position;
+    idoc['percent_xenon'] = req.body.Time;
+    idoc['tasks'] = req.body.Tasks;
+    idoc['mailing_lists'] = [req.body.mlist1, req.body.mlist2, req.body.mlist3]
+    idoc['mail_list_added'] = false;
+    if(req.body.StartDate != ""){
+        idoc['start_date'] = new Date(`${req.body.StartDate}`);
+    }
+    
     try {
-        db.collection('users').insertOne({
-            "first_name": firstName,
-            "last_name": lastName,
-            "email": userEmail,
-            "institute": instituteName,
-            "position": position,
-            "percent_xenon": time,
-            "tasks": tasks,
-            "mailing_lists": [req.body.mlist1, req.body.mlist2, req.body.mlist3],
-            "mail_list_added": false,
-            "start_date": start
-        })
-        console.log(`success. Added ${firstName} ${lastName}`)
-        res.redirect(`/institutes/${instituteName}`)
+        db.collection('users').insertOne(idoc)
+        console.log(`success. Added ${req.body.FirstName} ${req.body.LastName}`)
+        res.redirect(`/institutes/${req.body.Institute}`)
     } catch (e) {
         console.log(e)
     }
