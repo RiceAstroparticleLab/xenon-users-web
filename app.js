47fdeadf13931b55441662c945203b223f5ba71a
Yvette Martinez <ybm2@rice.edu>
2020-07-15 20:25:18 +0200

    fixing deployment issues

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var dotenv = require('dotenv')
 dotenv.config()
 // connect to the mongo server
 const MongoClient = require('mongodb').MongoClient
-const mongoURI = process.env.MONGOLAB_URI
+const mongoURI = process.env.DAQ_MONGO_URI
 var xenonnt_db
 var run_db
 var test_db
